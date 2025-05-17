0ebd4b9237119dfae7bb84b33298f68cc3c56560
nguyenluat19 <128703641+nguyenluat19@users.noreply.github.com>
2025-05-17 14:16:09 +0700

    Delete tenthanhvien/src directory

--- tenthanhvien/src/main/java/com/example/CategoryManager/Repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.CategoryManager.Repository;
-import com.example.CategoryManager.Category.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
