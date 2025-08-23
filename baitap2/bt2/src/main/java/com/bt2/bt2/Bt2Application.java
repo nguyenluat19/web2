65a5e641d99e3924623282064295f80e82e2f7f9
nguyenluat19 <128703641+nguyenluat19@users.noreply.github.com>
2025-08-23 10:20:33 +0700

    Update Bt2Application.java

--- baitap2/bt2/src/main/java/com/bt2/bt2/Bt2Application.java
+++ baitap2/bt2/src/main/java/com/bt2/bt2/Bt2Application.java
@@ -2,7 +2,7 @@ package com.bt2.bt2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//luật
 @SpringBootApplication
 public class Bt2Application {
 
